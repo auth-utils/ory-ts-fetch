5465f225f9f7eaec04bdbdc07b7d79f23570e780
Jorge <jkodex@gmail.com>
2023-12-09 21:46:41 +0100

    first commit

---
+++ src/src/apis/index.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CourierApi';
+export * from './FrontendApi';
+export * from './IdentityApi';
+export * from './JwkApi';
+export * from './MetadataApi';
+export * from './OAuth2Api';
+export * from './OidcApi';
+export * from './PermissionApi';
+export * from './ProjectApi';
+export * from './RelationshipApi';
+export * from './WellknownApi';
